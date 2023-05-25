cb36190187df4ee324a6c3c2a5aca97fe58775a1
LucasEmAru <lucasemanueldearaujo@gmail.com>
2023-05-25 20:00:25 -0300

    UC12
    Back End

--- Program.cs
+++ Program.cs
@@ -21,7 +21,11 @@ class Program {
    
    Console.ForegroundColor = ConsoleColor.DarkGreen; 
  BarraCarregamento();
- string opcao;
+ 
+ List<Fisica> listaPf = new List<Fisica>(); 
+ 
+string opcao;
+ 
 do{   
    Console.WriteLine(@$"
    ------------------------------------------------------------------------------------
@@ -37,26 +41,91 @@ do{
                                                                                 
 ");
 opcao = Console.ReadLine();
-
+ string opcaoPF;
+ 
+ 
    switch (opcao){
     case "1":
-    Endereco enderecoLucas = new Endereco(); 
-     enderecoLucas.Logradouro = "Cosme e Damião, Avenida 2";
-     enderecoLucas.Numero = 362;
-     enderecoLucas.Complemento = "Apartamentos/Condominios";
-     enderecoLucas.enderecoComercial = true;
 
-     Fisica novapf = new Fisica();
-     novapf.cpf = "000000000-00";
-     novapf.Endereco = enderecoLucas;
-     novapf.dataNascimento = new DateTime (2010, 05, 31 , 09 , 01 , 00 );
-     novapf.nome = "Lucas";
+    do{
+      Console.Clear();
+      Console.WriteLine(@$"
+   ------------------------------------------------------------------------------------
+   |                                                                                  |
+   |                          Escolha uma das opções abaixo                           |
+   |                                                                                  |
+   ------------------------------------------------------------------------------------
+                                                                                     
+                              1°- Cadastrar Pessoa Física                                  
+                              2°- Mostrar Pessoas Cadastradas                               
+                                          Voltar                                      
+                                                                                  
+                                                                                
+");
+     opcaoPF = Console.ReadLine();
+   switch (opcaoPF){
+       case "1":
+       Endereco endereco = new Endereco(); 
+       Fisica novapf = new Fisica();
 
-     Console.WriteLine("Opção escolhida: Pessoa Física." );
-     Console.WriteLine($"Usuario {novapf.nome} do CPF {novapf.cpf} Nascido em {novapf.dataNascimento}, Endereço {novapf.Endereco.Logradouro}, N° {novapf.Endereco.Numero}");
-     Console.WriteLine(novapf.Endereco.Complemento);
-     Console.WriteLine(novapf.Endereco.enderecoComercial);
+       Console.WriteLine("Digite o nome para cadastro.");
+       novapf.nome = Console.ReadLine();
+       Console.WriteLine("Digite o CPF.");
+       novapf.cpf = Console.ReadLine();
+       Console.WriteLine("Digite a data de nascimento. Ex:Dia/mês/Ano");
+       novapf.dataNascimento = DateTime.Parse(Console.ReadLine());
+       novapf.Endereco = endereco;
+       
+       Console.WriteLine("Digite o endereço para o cadastro.");
+       endereco.Logradouro = Console.ReadLine(); 
+       Console.WriteLine("Número da residencia.");
+       endereco.Numero = int.Parse(Console.ReadLine());
+       Console.WriteLine("Complemento.");
+       endereco.Complemento = Console.ReadLine(); 
+       Console.WriteLine("Comercial? SIM/NÃO.");
+       string endCom = Console.ReadLine().ToUpper();
+       
+       if(endCom == "SIM"){
+          endereco.enderecoComercial = true;
+       } else{ 
+          endereco.enderecoComercial = false;         
+       }
+
+       //listaPf.Add(novapf);
+       
+       
+       
+       using( StreamWriter sw = new StreamWriter($"{novapf.nome}")){
+               sw.WriteLine(novapf.nome);
+               sw.WriteLine(novapf.dataNascimento);
+       };
+       Console.WriteLine("Cadastro concluido");
+       Thread.Sleep(5000);
+       break;
+       case "2": 
+       Console.WriteLine("$ Quem você deseja consultar?");
+       string Registro = Console.ReadLine();
+       using(StreamReader sr = new StreamReader($"{Registro}.txt")){
+             string linha;
+             while ((linha = sr.ReadLine()) != null){
+               Console.WriteLine("{$ linha}");
+             };
+       };
+       Console.WriteLine("Precione qualquer tecla para continuar");
+       Console.ReadLine();
+       break;
+       case "Voltar":
+       break;
+       default:
+       Console.Clear();
+       Console.WriteLine("Opção Invalida, escolha uma das opções apresentadas.");
+       Thread.Sleep(2000);
+       break;
+   }
+     }while(opcaoPF != "Voltar");
+    
     break;
+    
     case "2":   
      Endereco enderecoCurtume = new Endereco(); 
      enderecoCurtume.Logradouro = "Rajada, Rua Joventina Jovem";
